9a83c7edfd1eed43ad3f88089104e808aa94aadb
Reagan Scoles <reagan.scoles@gmail.com>
2022-05-25 03:31:31 -0400

    Changed functionality drastically - much simpler
    I left the old code in, but it's commented out in case I use it later.
    
    Now, the bot moves forward 500 units, then turns 90, then moves forward again, rinse and repeat. Whenever it stops moving, it spins the scanner and gun 360 degrees. If it scans a bot, and that bot is too far away, it won't fire. If the bot is close enough, then it will fire. It will adjust the power of the shot depending on how close it is.

--- Project/Student Robot/fs_student/RSBot.java
+++ Project/Student Robot/fs_student/RSBot.java
@@ -26,10 +26,8 @@ public class RSBot extends TeamRobot
             this.setMaxVelocity(10);
 
             ahead(500);
-            turnRight(45);
+            turnRight(90);
             turnGunRight(360);
-            //back(500);
-            //turnGunRight(360);
 
             //stop(); - causes the robot to stop until resume() is called
             //resume(); - causes the robot to continue moving
@@ -43,65 +41,78 @@ public class RSBot extends TeamRobot
         //Power of 2 - Travels slower, but does more damage than 1
         //Power of 3 - Travels slowest, but does the most damage
 
-        if(getOthers() < 2) //If there's only one other bot left in the game
-        {
-            double enemyAngle = e.getBearing();
-            if(e.getDistance() < 300)
-            {
-                //double angleToFire = Math.toRadians(myStatus.getHeading() + enemyAngle % 360);
-                //double enemyXPos = (myStatus.getX() + Math.sin(angleToFire) * e.getDistance());
-                //double enemyYPos = (myStatus.getY() + Math.sin(angleToFire) * e.getDistance());
-                //turnGunRight(angleToFire);
-
-                turnRight(enemyAngle);
-                fire(2);
-            }
-            else
-            {
-                turnRight(enemyAngle);
-                ahead(200);
-                fire(1);
-            }
-        }
+        //if(getOthers() < 2) //If there's only one other bot left in the game
+        //{
+        //    double enemyAngle = e.getBearing();
+        //    if(e.getDistance() < 300)
+        //    {
+        //        //double angleToFire = Math.toRadians(myStatus.getHeading() + enemyAngle % 360);
+        //        //double enemyXPos = (myStatus.getX() + Math.sin(angleToFire) * e.getDistance());
+        //        //double enemyYPos = (myStatus.getY() + Math.sin(angleToFire) * e.getDistance());
+        //        //turnGunRight(angleToFire);
+//
+        //        turnRight(enemyAngle);
+        //        fire(2);
+        //    }
+        //    else
+        //    {
+        //        turnRight(enemyAngle);
+        //        ahead(200);
+        //        fire(1);
+        //    }
+        //}
 
         //If the robot is close, fire; Otherwise, don't fire
+        //if(e.getDistance() < 600)
+        //{
+        //    //Fire with a power of 1
+        //    fire(1);
+//
+        //    if(e.getDistance() < 300)
+        //    {
+        //        fire(2);
+        //    }
+        //    else if(e.getDistance() < 100)
+        //    {
+        //        fire(3);
+        //    }
+        //}
+
         if(e.getDistance() < 600)
         {
-            //Fire with a power of 1
             fire(1);
-
-            if(e.getDistance() < 300)
-            {
-                fire(2);
-            }
-            else if(e.getDistance() < 100)
-            {
-                fire(3);
-            }
+        }
+        else if(e.getDistance() < 300)
+        {
+            fire(2);
+        }
+        else if(e.getDistance() < 100)
+        {
+            fire(3);
         }
     }
 
     //onHitByBullet determines what the robot does when it's hit by a bullet
-    public void onHitByBullet(HitByBulletEvent e)
-    {
-        //Turn either left or right and start moving that way
-        Random rand = new Random();
-        //int upperbound = 1;
-        float randFlt = rand.nextFloat();
-        if(randFlt < 0.5)
-        {
-            turnLeft(70);
-        }
-        else if(randFlt >= 0.5)
-        {
-            turnRight(70);
-        }
-
-        //Increase speed to avoid being shot again
-        this.setMaxVelocity(20);
-        waitFor(new MoveCompleteCondition(this));   //When the move is complete, return to normal speed
-        this.setMaxVelocity(10);
-    }
+    //public void onHitByBullet(HitByBulletEvent e)
+    //{
+    //    //Turn either left or right and start moving that way
+    //    Random rand = new Random();
+    //    //int upperbound = 1;
+    //    float randFlt = rand.nextFloat();
+    //    if(randFlt < 0.5)
+    //    {
+    //        turnLeft(70);
+    //    }
+    //    else if(randFlt >= 0.5)
+    //    {
+    //        turnRight(70);
+    //    }
+//
+    //    //Increase speed to avoid being shot again
+    //    this.setMaxVelocity(20);
+    //    waitFor(new MoveCompleteCondition(this));   //When the move is complete, return to normal speed
+    //    this.setMaxVelocity(10);
+    //}
 
     //onHitWall determines what to do if the bot hits the wall
     public void onHitWall(HitWallEvent e)
@@ -121,15 +132,18 @@ public class RSBot extends TeamRobot
         //    turnRight(wallBearing);
         //}
 
-        if(forward == true)
-        {
-            back(100);
-            forward = false;
-        }
-        else
-        {
-            ahead(100);
-            forward = true;
-        }
+        //if(forward == true)
+        //{
+        //    back(100);
+        //    forward = false;
+        //}
+        //else
+        //{
+        //    ahead(100);
+        //    forward = true;
+        //}
+
+        //turnRight(90);
+        stop();
     }
 }
